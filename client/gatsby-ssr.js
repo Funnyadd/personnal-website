dee9d79578a031f90920b68653ae857840135e7d
Adam Mihajlovic <mihajlovicadam@gmail.com>
2022-12-30 19:29:56 -0500

    Revert "adding local support"
    This reverts commit ae9dfb2152e3b3828063bf35b079165b8cb7cb3a.

--- client/gatsby-ssr.js
+++ client/gatsby-ssr.js
@@ -7,29 +7,10 @@ import {
 } from "@apollo/client"
 import fetch from "isomorphic-fetch"
 
-const checkIfLocal = () => {
-    fetch(process.env.GATSBY_STRAPI_URL, { method: 'HEAD', signal: Timeout(2).signal })
-    .then(res => {
-        console.log('chu cool')
-        return process.env.GATSBY_STRAPI_URL
-    })
-    .catch(err => {
-        console.log('t cool')
-        return 'http://127.0.0.1:1338'
-    })
-    return 'http://127.0.0.1:1338'
-}
-
-const Timeout = (time) => {
-	let controller = new AbortController();
-	setTimeout(() => controller.abort(), time * 1000);
-	return controller;
-};
-
 const client = new ApolloClient({
     link: new HttpLink({
         fetch,
-        uri: `${checkIfLocal()}/graphql`,
+        uri: `${process.env.GATSBY_STRAPI_URL}/graphql`,
     }),
     cache: new InMemoryCache(),
 })
