0c31912d3d97d17e78d395188eb8861c487a0c00
Adam Mihajlovic <mihajlovicadam@gmail.com>
2024-07-19 15:23:58 -0400

    removed ugly spacing in new animated arrow component

--- client/src/components/AnimatedArrow/index.js
+++ client/src/components/AnimatedArrow/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { ArrowContainer, ArrowIcon } from "./AnimatedArrow"
 
 const AnimatedArrow = () => {
-    
     return (
         <ArrowContainer href="#About">
             <ArrowIcon size={56} color="white" />
